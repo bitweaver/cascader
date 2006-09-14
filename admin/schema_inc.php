6af71edf59f2e9314cd62a15d833785a9c2efe42
Max Kremmel <xing@synapse.plus.com>
2006-09-14 09:54:11 +0000

    add some generic package files

---
+++ admin/schema_inc.php
@@ -0,0 +1,7 @@
+<?php
+global $gBitInstaller;
+$gBitInstaller->registerPackageInfo( CASCADER_PKG_NAME, array(
+	'description' => "A site style creation assistant. Allows you to apply various colors to your site to help you decide on how you want to design your site.",
+	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
+) );
+?>
---
+++ templates/css_header.tpl
@@ -0,0 +1,12 @@
+/**
+ * Scheme Title:    {$scheme.title}
+ * Scheme Designer: Daily Color Scheme
+ *                  dailiycolorscheme.com
+ *
+ * Full list of colors in this theme:
+{foreach from=$scheme.colors item=color}
+ *       {$color}
+{/foreach}
+ */
+
+
